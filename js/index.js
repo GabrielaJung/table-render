7e8d0025fb9d61f0be08776552caa7ce12164c5b
GabrielaJung <gabrielamjung@gmail.com>
2024-06-27 21:28:21 -0300

    creating table with dblclick event to edit

--- js/index.js
+++ js/index.js
@@ -13,6 +13,20 @@ const jsonData = {
   },
 };
 
+const editableComponent = {
+  constructor() {
+    this.data = null;
+  },
+
+  getData() {
+    return this.data;
+  },
+
+  setData(value) {
+    this.data = value;
+  },
+};
+
 async function requestJson() {
   await fetch("data.json").then((response) => {
     response.json().then((data) => jsonData.setData(data));
@@ -23,52 +37,79 @@ function Table() {
   const tableElement = document.createElement("table");
 
   //header
-  const tHeader = document.createElement("theader");
-  const trHeader = document.createElement("tr");
-  const tdIdHeader = document.createElement("td");
-  const tdProductHeader = document.createElement("td");
-  const tdSoldHeader = document.createElement("td");
+  const tHeader = tableElement.createTHead();
 
-  tdIdHeader.textContent = "ID";
-  tdProductHeader.textContent = "Produto";
-  tdSoldHeader.textContent = "Quantidade vendida";
-  trHeader.append(tdIdHeader, tdProductHeader, tdSoldHeader);
-  tHeader.append(trHeader);
+  const trHeader = tHeader.insertRow(0);
+  trHeader.insertCell(0).textContent = "ID";
+  trHeader.insertCell(1).textContent = "Produto";
+  trHeader.insertCell(2).textContent = "Quantidade vendida";
 
   //body
-  const tBody = document.createElement("tbody");
-  tBody.append(TableBody());
+  const tBody = tableElement.createTBody();
+  TableBody({ tBody: tBody });
 
   tableElement.append(tHeader, tBody);
 
   return tableElement;
 }
 
-function TableBody() {
+function TableBody({ tBody }) {
   const data = jsonData.getData();
   console.log(data);
 
-  const tbody = data.map((productInfo) => {
-    const row = document.createElement("tr");
-    const colId = document.createElement("td");
-    const colProduct = document.createElement("td");
-    const colSold = document.createElement("td");
+  data.forEach((productInfo, index) => {
+    const row = tBody.insertRow(index);
 
-    colId.textContent = `${productInfo.id}`;
-    colProduct.textContent = `${productInfo.produto}`;
-    colSold.textContent = `${productInfo.vendidos}`;
+    const colId = row.insertCell(0);
+    const colProduct = row.insertCell(1);
+    const colSold = row.insertCell(2);
 
-    row.append(colId, colProduct, colSold);
-    return row;
+    colId.textContent = productInfo.id;
+    colProduct.textContent = productInfo.produto;
+    colSold.textContent = productInfo.vendidos;
+
+    // colId.setAttribute("id", productInfo.id);
+    // colProduct.setAttribute("id", productInfo.produto);
+    // colSold.setAttribute("id", productInfo.vendidos);
+
+    // dblclick event listener
+    colId.addEventListener("dblclick", () => {
+      colId.setAttribute("contenteditable", true);
+      editableComponent.setData(colId);
+
+      // console.log(parseInt(colId.textContent));
+    });
+
+    colProduct.addEventListener("dblclick", () => {
+      colProduct.setAttribute("contenteditable", true);
+      editableComponent.setData(colProduct);
+
+      // console.log(colProduct.textContent);
+    });
+
+    colSold.addEventListener("dblclick", () => {
+      colSold.setAttribute("contenteditable", true);
+      editableComponent.setData(colSold);
+
+      // console.log(parseInt(colSold.textContent));
+    });
+
+    // when click out editable component, remove permission to edit it
+    document.onclick = function (e) {
+      const editableCell = editableComponent.getData();
+      if (!editableCell) return;
+      if (e.target !== editableCell) {
+        editableCell.setAttribute("contenteditable", false);
+        editableComponent.setData(null);
+      }
+    };
   });
-  console.log(tbody);
-  return tbody;
+  return tBody;
 }
 
 function init() {
   const data = jsonData.getData();
   document.querySelector("body").prepend(Table());
-  console.log(data);
 }
 
 requestJson();
